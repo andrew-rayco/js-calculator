3072f9d6e05d82fc8b69c18f8575720bf8720baa
Andrew <andy@andycondon.com>
2017-04-11 13:42:42 +1200

    make all buttons work

--- js/calculator.js
+++ js/calculator.js
@@ -1,66 +1,70 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  var display = document.getElementsByClassName('screen')[0];
+  var mainDisplay = document.getElementsByClassName('main')[0];
+  var subDisplay = document.getElementsByClassName('sub')[0];
   var keys = document.getElementsByClassName('keys')[0];
   var historyString = '';
-  var total;
+  var historyArray = [];
 
   // display the right number when clicked
   keys.addEventListener('click', countHistory);
 
   function countHistory(evt) {
-
     switch(evt.target.id) {
+      case 'blank':
+        break;
       case 'all-cancel':
         historyString = '';
-        display.innerHTML = historyString;
+        showHistory();
         break;
       case 'cancel-entry':
-        alert('cancel-entry');
-        display.innerHTML = historyString;
+        cancelEntry();
         break;
       case 'divide':
+        historyArray.push('/');
         historyString += '/';
-        display.innerHTML = historyString;
+        showHistory();
         break;
       case 'multiply':
+        historyArray.push('*');
         historyString += '*';
-        display.innerHTML = historyString;
+        showHistory();
         break;
       case 'minus':
+        historyArray.push('-');
         historyString += '-';
-        display.innerHTML = historyString;
+        showHistory();
         break;
       case 'plus':
+        historyArray.push('+');
         historyString += '+';
-        display.innerHTML = historyString;
+        showHistory();
         break;
       case 'equals':
         total();
         break;
       default:
+        historyArray.push(evt.target.id);
         historyString += parseInt(evt.target.id);
-        display.innerHTML = historyString;
+        showHistory();
+        break;
     }
-    // console.log(parseInt(evt.target.id));
-
-
-    function total() {
-      display.innerHTML = (eval(historyString));
-      console.log(eval(historyString));
-    }
-
-    // displayCalc(historyString);
   }
 
-  // function displayCalc(history) {
-  //
-  //   display.innerHTML = history;
-  // }
+  function cancelEntry() {
+    historyArray.pop();
+    historyString = historyArray.join('');
+    showHistory();
+  }
 
+  function showHistory() {
+    mainDisplay.innerHTML = historyString;
+    subDisplay.innerHTML = historyString;
+  }
 
-
-
-
+  function total() {
+    mainDisplay.innerHTML = (eval(historyString));
+    subDisplay.innerHTML = historyString + " = " + (eval(historyString));
+  }
 
 }); //close DOMContentLoaded
